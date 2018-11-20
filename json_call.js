ea3b26cce32357bffe72f945e0f900197d65a811
timi <timblir@gmail.com>
2018-11-20 13:59:33 +0200

    more to embed js

--- json_call.js
+++ json_call.js
@@ -6,14 +6,45 @@ xmlhttp.onreadystatechange = function() {
         console.log('responseText:' + xmlhttp.responseText);
         var parsedJSON = JSON.parse(xmlhttp.responseText);
         console.log(parsedJSON.label_text_fsize, "FONT SIZE NIGGERS");
+
         var crtPara = document.createElement("p");
-        var crtDiv = document.createElement("div");
-        var crtSpan = document.createElement("span");
+        var crtDivMain = document.createElement("div");
+        crtDivMain.setAttribute("id", "main_div");
+        crtDivMain.className = "main_div";
+        var crtDivLabel = document.createElement("div");
+        crtDivLabel.setAttribute("id", "label_div");
+        crtDivLabel.className = "label_div";
+        var crtDivLabelInner = document.createElement("div");
+        crtDivLabelInner.setAttribute("id", "label_inner_div");
+        crtDivLabelInner.className = "label_inner_div";
+        var crtDivSummary = document.createElement("div");
+        crtDivSummary.setAttribute("id", "summary_div");
+        crtDivSummary.className = "summary_div";
+        var crtDivExit = document.createElement("div");
+        crtDivExit.setAttribute("id", "exit_div");
+        crtDivExit.className = "exit_div";
+        var crtBtn = document.createElement("button");
+        crtBtn.setAttribute("id", "button_div");
+        crtBtn.className = "button_div";
+
+
+        document.body.appendChild(crtDivMain);
+        document.getElementById("main_div").appendChild(crtDivLabel);
+        document.getElementById("label_div").appendChild(crtDivLabelInner);
+        crtDivLabelInner.innerText = parsedJSON.label_text;
+        document.getElementById("main_div").appendChild(crtDivSummary);
+        crtDivSummary.innerText = parsedJSON.summary;
+        document.getElementById("main_div").appendChild(crtDivExit);
+        document.getElementById("exit_div").appendChild(crtBtn);
+
+
+        crtPara.setAttribute("id", "main_div");
+        // document.body.appendChild(ctrDiv.setAttribute("id", "main_div"));
         document.body.appendChild(crtPara);
         crtPara.innerText = "Lorem Ipsum Dolor Aisgmet Bagati-ai in el scris";
         crtPara.style.fontSize = parsedJSON.label_text_fsize;
     }
 };
 
-xmlhttp.open("GET", "./Json/46/data.json", true);
+xmlhttp.open("GET", "./Json/43/data.json", true);
 xmlhttp.send();
